d44adf4c2a66ce1b88867561784dcd6e0950eb6d
wlm1455925277 <87473483+wlm1455925277@users.noreply.github.com>
2026-02-02 09:17:21 +0800

    完全汉化

--- API/src/main/java/su/nightexpress/excellentenchants/api/item/ItemSetDefaults.java
+++ API/src/main/java/su/nightexpress/excellentenchants/api/item/ItemSetDefaults.java
@@ -13,30 +13,30 @@ import java.util.stream.Stream;
 
 public enum ItemSetDefaults {
 
-    HELMET(tagLookup -> ItemSet.buildByName("helmet", tagLookup.getHelmets()).slots(EquipmentSlot.HEAD).name("Helmet").build()),
-    CHESTPLATE(tagLookup -> ItemSet.buildByName("chestplate", tagLookup.getChestplates()).slots(EquipmentSlot.CHEST).name("Chestplate").build()),
-    LEGGINGS(tagLookup -> ItemSet.buildByName("leggings", tagLookup.getLeggings()).slots(EquipmentSlot.LEGS).name("Leggings").build()),
-    BOOTS(tagLookup -> ItemSet.buildByName("boots", tagLookup.getBoots()).slots(EquipmentSlot.FEET).name("Boots").build()),
-    ELYTRA(tagLookup -> ItemSet.buildByType("elytra", Material.ELYTRA).slots(EquipmentSlot.CHEST).name("Elytra").build()),
-    SWORD(tagLookup -> ItemSet.buildByName("sword", tagLookup.getSwords()).slots(EquipmentSlot.HAND).name("Sword").build()),
-    AXE(tagLookup -> ItemSet.buildByName("axe", tagLookup.getAxes()).slots(EquipmentSlot.HAND).name("Axe").build()),
-    HOE(tagLookup -> ItemSet.buildByName("hoe", tagLookup.getHoes()).slots(EquipmentSlot.HAND).name("Hoe").build()),
-    PICKAXE(tagLookup -> ItemSet.buildByName("pickaxe", tagLookup.getPickaxes()).slots(EquipmentSlot.HAND).name("Pickaxe").build()),
-    SHOVEL(tagLookup -> ItemSet.buildByName("shovel", tagLookup.getShovels()).slots(EquipmentSlot.HAND).name("Shovel").build()),
-    TRIDENT(tagLookup -> ItemSet.buildByType("trident", Material.TRIDENT).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("Trident").build()),
-    BOW(tagLookup -> ItemSet.buildByType("bow", Material.BOW).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("Bow").build()),
-    CROSSBOW(tagLookup -> ItemSet.buildByType("crossbow", Material.CROSSBOW).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("Crossbow").build()),
-    FISHING_ROD(tagLookup -> ItemSet.buildByType("fishing_rod", Material.FISHING_ROD).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("Fishing Rod").build()),
-    SHIELD(tagLookup -> ItemSet.buildByType("shield", Material.SHIELD).slots(EquipmentSlot.OFF_HAND).name("Shield").build()),
-    BREAKABLE(tagLookup -> ItemSet.buildByName("breakable", tagLookup.getBreakable()).slots(EquipmentSlot.values()).name("Brekable").build()),
-    ARMOR(tagLookup -> ItemSet.buildByName("armor", getArmors(tagLookup)).slots(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET).name("Armor").build()),
-    TOOL(tagLookup -> ItemSet.buildByName("tool", getTools(tagLookup)).slots(EquipmentSlot.HAND).name("Tool").build()),
-    SWORDS_AXES(tagLookup -> ItemSet.buildByName("swords_axes", getSwordsAxes(tagLookup)).slots(EquipmentSlot.HAND).name("Weapon").build()),
-    BOW_CROSSBOW(tagLookup -> ItemSet.buildByType("bow_crossbow", Lists.newSet(Material.BOW, Material.CROSSBOW)).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("Bow / Crossbow").build()),
-    CHESTPLATE_ELYTRA(tagLookup -> ItemSet.buildByName("chestplate_elytra", getTorso(tagLookup)).slots(EquipmentSlot.CHEST).name("Chestplate / Elytra").build()),
-    ALL_WEAPON(tagLookup -> ItemSet.buildByName("weapon", getAllRangeWeapon(tagLookup)).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("Weapon").build()),
-    MINING_TOOLS(tagLookup -> ItemSet.buildByName("mining_tools", getMiningTools(tagLookup)).slots(EquipmentSlot.HAND).name("Mining Tool").build()),
-    TOOLS_WEAPONS(tagLookup -> ItemSet.buildByName("tools_weapons", getToolsAndWeapon(tagLookup)).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("Tools & Weapons").build())
+    HELMET(tagLookup -> ItemSet.buildByName("helmet", tagLookup.getHelmets()).slots(EquipmentSlot.HEAD).name("头盔").build()),
+    CHESTPLATE(tagLookup -> ItemSet.buildByName("chestplate", tagLookup.getChestplates()).slots(EquipmentSlot.CHEST).name("胸甲").build()),
+    LEGGINGS(tagLookup -> ItemSet.buildByName("leggings", tagLookup.getLeggings()).slots(EquipmentSlot.LEGS).name("护腿").build()),
+    BOOTS(tagLookup -> ItemSet.buildByName("boots", tagLookup.getBoots()).slots(EquipmentSlot.FEET).name("靴子").build()),
+    ELYTRA(tagLookup -> ItemSet.buildByType("elytra", Material.ELYTRA).slots(EquipmentSlot.CHEST).name("鞘翅").build()),
+    SWORD(tagLookup -> ItemSet.buildByName("sword", tagLookup.getSwords()).slots(EquipmentSlot.HAND).name("剑").build()),
+    AXE(tagLookup -> ItemSet.buildByName("axe", tagLookup.getAxes()).slots(EquipmentSlot.HAND).name("斧").build()),
+    HOE(tagLookup -> ItemSet.buildByName("hoe", tagLookup.getHoes()).slots(EquipmentSlot.HAND).name("锄").build()),
+    PICKAXE(tagLookup -> ItemSet.buildByName("pickaxe", tagLookup.getPickaxes()).slots(EquipmentSlot.HAND).name("镐").build()),
+    SHOVEL(tagLookup -> ItemSet.buildByName("shovel", tagLookup.getShovels()).slots(EquipmentSlot.HAND).name("铲").build()),
+    TRIDENT(tagLookup -> ItemSet.buildByType("trident", Material.TRIDENT).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("三叉戟").build()),
+    BOW(tagLookup -> ItemSet.buildByType("bow", Material.BOW).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("弓").build()),
+    CROSSBOW(tagLookup -> ItemSet.buildByType("crossbow", Material.CROSSBOW).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("弩").build()),
+    FISHING_ROD(tagLookup -> ItemSet.buildByType("fishing_rod", Material.FISHING_ROD).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("钓鱼竿").build()),
+    SHIELD(tagLookup -> ItemSet.buildByType("shield", Material.SHIELD).slots(EquipmentSlot.OFF_HAND).name("盾牌").build()),
+    BREAKABLE(tagLookup -> ItemSet.buildByName("breakable", tagLookup.getBreakable()).slots(EquipmentSlot.values()).name("可损坏物品").build()),
+    ARMOR(tagLookup -> ItemSet.buildByName("armor", getArmors(tagLookup)).slots(EquipmentSlot.HEAD, EquipmentSlot.CHEST, EquipmentSlot.LEGS, EquipmentSlot.FEET).name("护甲").build()),
+    TOOL(tagLookup -> ItemSet.buildByName("tool", getTools(tagLookup)).slots(EquipmentSlot.HAND).name("工具").build()),
+    SWORDS_AXES(tagLookup -> ItemSet.buildByName("swords_axes", getSwordsAxes(tagLookup)).slots(EquipmentSlot.HAND).name("武器").build()),
+    BOW_CROSSBOW(tagLookup -> ItemSet.buildByType("bow_crossbow", Lists.newSet(Material.BOW, Material.CROSSBOW)).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("弓 / 弩").build()),
+    CHESTPLATE_ELYTRA(tagLookup -> ItemSet.buildByName("chestplate_elytra", getTorso(tagLookup)).slots(EquipmentSlot.CHEST).name("胸甲 / 鞘翅").build()),
+    ALL_WEAPON(tagLookup -> ItemSet.buildByName("weapon", getAllRangeWeapon(tagLookup)).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("武器").build()),
+    MINING_TOOLS(tagLookup -> ItemSet.buildByName("mining_tools", getMiningTools(tagLookup)).slots(EquipmentSlot.HAND).name("挖掘工具").build()),
+    TOOLS_WEAPONS(tagLookup -> ItemSet.buildByName("tools_weapons", getToolsAndWeapon(tagLookup)).slots(EquipmentSlot.HAND, EquipmentSlot.OFF_HAND).name("工具与武器").build())
     ;
 
     public static void initializeAll(@NotNull ItemTagLookup tagLookup) {
@@ -140,7 +140,7 @@ public enum ItemSetDefaults {
     @NotNull
     public ItemSet getItemSet() {
         if (this.itemSet == null) {
-            throw new IllegalStateException("ItemSet 尚未初始化！");
+            throw new IllegalStateException("物品套装尚未初始化！");
         }
         return this.itemSet;
     }
